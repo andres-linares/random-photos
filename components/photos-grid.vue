cf8671a5f20397d7278651ef8f3855ac2512e54f
andres-linares2211 <andres.linares@omni.pro>
2021-12-07 18:57:35 -0500

    feat: more things

---
+++ components/photos-grid.vue
@@ -0,0 +1,91 @@
+<template>
+  <ul>
+    <li
+      v-for="seed in seeds"
+      :key="seed.seed"
+      :style="{ 'grid-row-end': `span ${seed.span}` }"
+    >
+      <img :src="`https://picsum.photos/seed/${seed.seed}/${imageSize}`" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      seeds: [],
+      interval: null,
+      imageSize: 600,
+    };
+  },
+  created() {
+    this.imageSize = window.innerWidth < 720 ? 350 : 600;
+  },
+  mounted() {
+    this.interval = setInterval(
+      () => {
+        const seed = Math.random() * (this.seeds.length + 1);
+        let span = Math.round(Math.random() * 2 + 1);
+        const lastSpan = this.seeds[this.seeds.length - 1]?.span;
+        if (span === lastSpan) span = Math.round(Math.random() * 2 + 1);
+
+        this.seeds.push({ seed, span });
+
+        this.$nextTick().then(() => {
+          window.scrollTo({ top: 999999, behavior: "smooth" });
+        });
+      },
+      window.innerWidth < 720 ? 900 : 750
+    );
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
+  grid-auto-rows: 6rem;
+  gap: 1rem;
+  margin: 1rem;
+  margin-left: 1.125rem;
+}
+
+@media screen and (max-width: 720px) {
+  ul {
+    grid-template-columns: repeat(auto-fill, minmax(8rem, 1fr));
+    grid-auto-rows: 4rem;
+  }
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  box-sizing: border-box;
+  background: linear-gradient(45deg, #ffffff1a, #dddddd1a);
+  opacity: 0;
+  animation: appear 700ms ease-out 1s forwards;
+}
+
+li {
+  position: relative;
+  box-shadow: 1px 1px 3px #ffffff46;
+}
+
+li::after {
+  content: "";
+  position: absolute;
+  inset: 0;
+  border: 4px solid #ffffffdf;
+  opacity: 0;
+  animation: appear 700ms ease-out forwards;
+}
+
+@keyframes appear {
+  to {
+    opacity: 1;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,53 @@
 <template>
-  <Tutorial/>
+  <div>
+    <photos-grid />
+
+    <span>Made with love by Andrés</span>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  head() {
+    return {
+      title: 'Random Photos',
+      description: 'A museum of random photos in a masonry style'
+    }
+  }
+};
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background: #18191a;
+  overflow-y: scroll;
+}
+
+span {
+  position: fixed;
+  top: 0.5rem;
+  left: 0.125rem;
+  font-family: 'Nunito';
+  color: #fff;
+  writing-mode: vertical-lr;
+  font-size: 0.6rem;
+  letter-spacing: 0.07em;
+}
+
+ul,
+ol {
+  list-style: none;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+}
+</style>
